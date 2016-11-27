67aa2b99b0d1f9f1fd2f028d6d4a4b409506c457
Martin Podlubny <martin.podlubny@gmail.com>
2016-11-27 15:31:34 +0000

    major re-haul around actual structure etc of the lightbulb to a package

---
+++ home-components/pi-bulb-checker/bulb/tests/bulb_tests.py
@@ -0,0 +1,215 @@
+import mockito
+import unittest
+from .. import Bulb
+from .. import BulbStates
+
+
+class BulbTests(unittest.TestCase):
+    def setUp(self):
+        self.home_sensor = mockito.mock()
+        self.desk_sensor = mockito.mock()
+        self.bed_sensor = mockito.mock()
+        self.light_sensor = mockito.mock()
+
+    def setReturnValues(self, bools):
+        mockito.when(self.home_sensor).isHome().thenReturn(bools[0])
+        mockito.when(self.desk_sensor).isAround().thenReturn(bools[1])
+        mockito.when(self.bed_sensor).isAround().thenReturn(bools[2])
+        mockito.when(self.light_sensor).isOn().thenReturn(bools[3])
+
+    def test_kitchen_lamp_wastedNotAround(self):
+        # ARRANGE
+        self.setReturnValues((True, True, False, True))
+
+        bulb = Bulb('Kitchen',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.kitchen_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_AROUND
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_kitchen_lamp_wastedInBed(self):
+        # ARRANGE
+        self.setReturnValues((True, False, True, True))
+
+        bulb = Bulb('Kitchen',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.kitchen_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.IN_BED
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_kitchen_lamp_wastedNotHome(self):
+        # ARRANGE
+        self.setReturnValues((False, False, True, True))
+
+        bulb = Bulb('Kitchen',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.kitchen_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_HOME
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_kitchenLampNotWasted(self):
+        # ARRANGE
+        self.setReturnValues((True, False, True, False))
+
+        bulb = Bulb('Kitchen',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.kitchen_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = False, BulbStates.OFF
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_desk_lamp_wastedNotAround(self):
+        # ARRANGE
+        self.setReturnValues((True, False, False, True))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_AROUND
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_desk_lamp_wastedNotHome(self):
+        # ARRANGE
+        self.setReturnValues((False, False, False, True))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_HOME
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_deskLampNotWasted(self):
+        # ARRANGE
+        self.setReturnValues((True, True, False, True))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = False, BulbStates.NOT_WASTED
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_deskLampOff(self):
+        # ARRANGE
+        self.setReturnValues((True, True, False, False))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = False, BulbStates.OFF
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_desk_lamp_wastedNotAround(self):
+        # ARRANGE
+        self.setReturnValues((True, False, False, True))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_AROUND
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_desk_lamp_wastedNotHome(self):
+        # ARRANGE
+        self.setReturnValues((False, True, False, True))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = True, BulbStates.NOT_HOME
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
+
+    def test_bedroomLampOff(self):
+        # ARRANGE
+        self.setReturnValues((True, True, False, False))
+
+        bulb = Bulb('Desk',
+                    self.light_sensor,
+                    [self.desk_sensor, self.bed_sensor],
+                    self.home_sensor,
+                    Bulb.desk_lamp_wasted)
+
+        # ACT
+        actual_is_wasted, actual_bulb_state = bulb.is_wasted()
+
+        # ASSERT
+        expected_is_wasted, expected_bulb_state = False, BulbStates.OFF
+        self.assertEqual(actual_is_wasted, expected_is_wasted)
+        self.assertEqual(actual_bulb_state, expected_bulb_state)
