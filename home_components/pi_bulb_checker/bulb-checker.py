d9311c8abe959bb635f182b7dd57e239554e2537
Martin Podlubny <martin.podlubny@gmail.com>
2016-11-29 18:15:38 +0000

    PEP8 cleanup

--- home_components/pi_bulb_checker/bulb-checker.py
+++ home_components/pi_bulb_checker/bulb-checker.py
@@ -1,17 +1,17 @@
 import urllib2
 
-class BulbChecker(object):
 
+class BulbChecker(object):
     def __init__(self, bulbs):
         self.bulbs = bulbs
 
     def submit_datapoint(self, bulbName, state):
-        ''' Submits this datapoint into the db '''
-        return urllib2.urlopen("http://localhost:8555/submit-bulb-datapoint?bulbName="+bulbName+"&state="+str(state))
-
+        """ Submits this datapoint into the db """
+        return urllib2.urlopen(
+            "http://localhost:8555/submit-bulb-datapoint?bulbName=" + bulbName + "&state=" + str(state))
 
     def check_bulbs(self):
-        ''' Checks each bulb and submits the data. '''
+        """ Checks each bulb and submits the data. """
         for bulb in self.bulbs:
             wasted, state = bulb.wastage_function()
-            self.submit_datapoint(bulb.name,state)
+            self.submit_datapoint(bulb.name, state)
