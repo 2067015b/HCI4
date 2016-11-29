0f89cf26e07e1900dc89693c2514b3ad327b32cf
Martin Podlubny <martin.podlubny@gmail.com>
2016-11-29 15:19:11 +0000

    list indices now ints instead of strings

--- home_components/pi_sensors_server/sensors_server.py
+++ home_components/pi_sensors_server/sensors_server.py
@@ -14,12 +14,12 @@ presence_sensors = [
 
 @route('/lamp-sensor/<lamp_sensor_index>')
 def get_lamp_state(lamp_sensor_index):
-    return str(lamp_sensors[lamp_sensor_index].is_on())
+    return str(lamp_sensors[int(lamp_sensor_index)].is_on())
 
 
 @route('/presence-sensor/<presence_sensor_index>')
 def get_lamp_state(presence_sensor_index):
-    return str(presence_sensors[presence_sensor_index].is_around())
+    return str(presence_sensors[int(presence_sensor_index)].is_around())
 
 # This actually spins up the server and it starts listening. To listen on 0.0.0.0 you might need root privileges
 run(host='0.0.0.0', port=8666, debug=True)
