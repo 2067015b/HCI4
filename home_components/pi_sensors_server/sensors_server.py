49b3deb044a91d53f85a29350e6b4a1a624ac418
Martin Podlubny <martin.podlubny@gmail.com>
2016-11-29 15:01:27 +0000

    added casts to str cuz I'm suspecting bottle needs them to return strings

--- home_components/pi_sensors_server/sensors_server.py
+++ home_components/pi_sensors_server/sensors_server.py
@@ -14,12 +14,12 @@ furnitures = {
 
 @route('/lamp/<lamp_name>')
 def get_lamp_state(lamp_name):
-    return lamps[lamp_name].is_on()
+    return str(lamps[lamp_name].is_on())
 
 
 @route('/furniture/<furniture_name>')
 def get_lamp_state(furniture_name):
-    return furnitures[furniture_name].is_around()
+    return str(furnitures[furniture_name].is_around())
 
 # This actually spins up the server and it starts listening. To listen on 0.0.0.0 you might need root privileges
 run(host='0.0.0.0', port=8666, debug=True)
